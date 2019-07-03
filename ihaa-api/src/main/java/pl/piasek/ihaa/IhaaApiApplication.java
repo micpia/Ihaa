cab2aeaeee862b530e01fad51cfaa49ea11f9e85
mpiasek <piasekm95@gmail.com>
2019-07-03 13:18:59 +0200

    Add spring boot api project, H2 init

---
+++ ihaa-api/src/main/java/pl/piasek/ihaa/IhaaApiApplication.java
@@ -0,0 +1,13 @@
+package pl.piasek.ihaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhaaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IhaaApiApplication.class, args);
+    }
+
+}
---
+++ ihaa-api/src/test/java/pl/piasek/ihaa/IhaaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.piasek.ihaa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IhaaApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
