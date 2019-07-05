229282d381f632213d2408b13fa61e74af2ffa19
mpiasek <piasekm95@gmail.com>
2019-07-05 17:50:13 +0200

    db seeder work in progress - draft version

--- ihaa-api/src/main/java/pl/piasek/ihaa/db/DbSeeder.java
+++ ihaa-api/src/main/java/pl/piasek/ihaa/db/DbSeeder.java
@@ -30,81 +30,160 @@ public class DbSeeder implements CommandLineRunner {
         this.countriesRepo = countriesRepo;
     }
 
-    private ArrayList<String> getFields(int row, int rowsInSheet, String field, String path) {
-        JSONParser jsonParser = new JSONParser();
+    private void readAndAppendFile(String path, int numberOfRuns, String competitionName) {
 
-        ArrayList<String> list = new ArrayList<>();
+        int ridersRow = 1;                   //some constant data about input table to json format
+        int countriesRow = 2;
+        int horsesRow = 3;
+        int stylesRow = 4;
+        String dataField = "FIELD2";
+
+        int rowsInSheet = numberOfRuns + 7;
+
+
+        //competition
+        Long competitionId = null;
+        //TODO competition data input to database
+
+        JSONParser jsonParser = new JSONParser();
 
         try (FileReader fileReader = new FileReader(path)) {
 
             Object object = jsonParser.parse(fileReader);
-            JSONArray scoresheets = (JSONArray) object;
+            JSONArray scoreSheets = (JSONArray) object;
+
 //            System.out.println(scoresheets);
 //            System.out.println("size: " + scoresheets.size());
             int sheetIterator = 0;
-            Iterator iterator = scoresheets.iterator();
+            Iterator iterator = scoreSheets.iterator();
 
-            while (iterator.hasNext()) {
+            while (iterator.hasNext()) {            //loop thru all scoresheets in file
 
                 sheetIterator++;
                 Iterator<Map.Entry> rowIterator = ((Map) iterator.next()).entrySet().iterator();
 
-                while (rowIterator.hasNext()) {
-                    if (sheetIterator % rowsInSheet == row) {
+                while (rowIterator.hasNext()) {     //loop thru one sheet
 
+                    //countries
+                    Long countriesId = null;
+                    String countriesName = null;
+
+                    //riders
+                    Long ridersId = null;
+                    String ridersName = null;
+                    String ridersSurname = null;
+                    //Long ridersCountriesId = countriesId;
+                    //Long ridersClubsId = null;           // no clubs data in scoresheet
+
+                    //horses
+                    Long horsesId = null;
+                    String horsesName = null;
+
+                    //TODO styles variable out of the loop const for all file, input file from only one style. Maybe pass styles data with argument
+                    //styles
+                    Long stylesId = null;
+                    String stylesName = null;
+                    float stylesPointsPerSecond = 1;
+
+                    //tracks
+                    Long tracksId = null;
+                    String tracksName = null;
+                    float tracksTimeLimit = 1;
+                    //Long tracksStylesId = stylesId;
+
+                    //starts
+                    Long startsId;
+                    //Long startsRidersId = ridersId;
+                    //Long startsCompetitionsId = competitionsId;
+                    //Long startsHorsesId = horsesId;
+
+                    if(sheetIterator % rowsInSheet == ridersRow) {       //riders table data
                         Map.Entry pair = rowIterator.next();
-
-                        if (pair.getKey().equals(field) && !list.contains(pair.getValue().toString())) {
-                            list.add(pair.getValue().toString());
+                        if(pair.getKey().equals(dataField)) {
+                            String fullName = pair.getValue().toString();
+                            int space = fullName.indexOf(" ");
+                            ridersName = fullName.substring(0,space);
+                            ridersSurname = fullName.substring(space + 1);
+                        } else {
+                            rowIterator.next();
                         }
-                    } else {
-                        rowIterator.next();
                     }
+
+                    if(sheetIterator % rowsInSheet == countriesRow) {       //countries
+                        Map.Entry pair = rowIterator.next();
+                        if(pair.getKey().equals(dataField)) {
+                            countriesName = pair.getValue().toString();
+                        } else {
+                            rowIterator.next();
+                        }
+                    }
+
+                    if(sheetIterator % rowsInSheet == horsesRow) {       //horses table data
+                        Map.Entry pair = rowIterator.next();
+                        if(pair.getKey().equals(dataField)) {
+                            horsesName = pair.getValue().toString();
+                        } else {
+                            rowIterator.next();
+                        }
+                    }
+
+                    if(sheetIterator % rowsInSheet == stylesRow) {       //styles table data
+                        Map.Entry pair = rowIterator.next();
+                        if(pair.getKey().equals(dataField)) {
+                            stylesName = pair.getValue().toString();
+                        } else {
+                            rowIterator.next();
+                        }
+                    }
+
+                    //TODO input countries, horses, riders, styles, starts data to db and get IDs
+
+                    if(sheetIterator % rowsInSheet > stylesRow && sheetIterator % rowsInSheet < stylesRow + numberOfRuns) {       //runs and shots
+
+                        //TODO manage each run data input with use of earlier fetched IDs
+                    }
+                        //just for reference
+//                    if (sheetIterator % rowsInSheet == row) {
+//
+//                        Map.Entry pair = rowIterator.next();
+//
+//                        //if (pair.getKey().equals(field) && !list.contains(pair.getValue().toString())) {
+//                        //    list.add(pair.getValue().toString());
+//                        //}
+//                    } else {
+//                        rowIterator.next();
+//                    }
+
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
-
-        return list;
     }
-
-    private void appendArray(ArrayList<String> array) {
-
-        List<Countries> countries = new ArrayList<>();
-
-        int count = 0;
-        while(array.size() > count) {
-
-            System.out.println(array.get(count));
-            if (!countriesRepo.existsByName(array.get(count))) {
-                countries.add(new Countries(array.get(count)));
-            }
-
-            count ++;
-        }
-        countriesRepo.saveAll(countries);
-    }
+//reference for faster changes in code, delete later
+//    private void appendArray(ArrayList<String> array) {
+//
+//        List<Countries> countries = new ArrayList<>();
+//
+//        int count = 0;
+//        while(array.size() > count) {
+//
+//            System.out.println(array.get(count));
+//            if (!countriesRepo.existsByName(array.get(count))) {
+//                countries.add(new Countries(array.get(count)));
+//            }
+//
+//            count ++;
+//        }
+//        countriesRepo.saveAll(countries);
+//    }
 
     @Override
     public void run(String... args) throws Exception {
 
 
-
-
         //countries.add(new Countries("Hungary"));
-
-        ArrayList<String> countries = getFields(2, 16, "FIELD2", "/home/michal/IdeaProjects/Ihaa/ihaa-api/src/main/resources/static/data/json/scoresheet_h1.json");
-
-        System.out.println("Fetched string array: " + countries);
-
-        appendArray(countries);
-
-
+//        ArrayList<String> countries = getFields(2, 16, "FIELD2", "/home/michal/IdeaProjects/Ihaa/ihaa-api/src/main/resources/static/data/json/scoresheet_h1.json");
+//        System.out.println("Fetched string array: " + countries);
+//        appendArray(countries);
     }
 
 
