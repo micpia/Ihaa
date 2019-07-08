760c8d30ec380261a2748676d29a5652f432b08d
mpiasek <piasekm95@gmail.com>
2019-07-08 14:04:46 +0200

    Fix typo

--- ihaa-api/src/main/java/pl/piasek/ihaa/db/ShotsRepo.java
+++ ihaa-api/src/main/java/pl/piasek/ihaa/db/ShotsRepo.java
@@ -3,5 +3,5 @@ package pl.piasek.ihaa.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.piasek.ihaa.model.Shots;
 
-public interface shotsRepo extends JpaRepository<Shots, Long> {
+public interface ShotsRepo extends JpaRepository<Shots, Long> {
 }
