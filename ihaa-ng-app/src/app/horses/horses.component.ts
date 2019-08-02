c4332ad6eca7ad8f6a64d1994d8e06ca6196c77b
mpiasek <piasekm95@gmail.com>
2019-08-02 11:20:37 +0200

    Add displaying horses table

--- ihaa-ng-app/src/app/horses/horses.component.ts
+++ ihaa-ng-app/src/app/horses/horses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../shared/api.service";
+import {Horse} from "../model/horse";
 
 @Component({
   selector: 'app-horses',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorsesComponent implements OnInit {
 
-  constructor() { }
+  horses: Horse[] = [];
+
+  constructor(private apiService:ApiService) { }
 
   ngOnInit() {
+    this.getAllHorses();
+  }
+
+  public getAllHorses() {
+    this.apiService.getAllHorses().subscribe(
+      res => {
+        this.horses = res;
+      },
+      error => {
+        alert("Error has occurred while fetching horses table.")
+      }
+    )
   }
 
 }
--- ihaa-ng-app/src/app/shared/api.service.ts
+++ ihaa-ng-app/src/app/shared/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {RidersView} from "../model/riders-view";
+import {Horse} from "../model/horse";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,15 @@ export class ApiService {
 
   private BASE_URL = "http://localhost:8080/api";
   private ALL_RIDERS_URL = `${this.BASE_URL}\\ridersView\\all`;
+  private ALL_HORSES_URL = `${this.BASE_URL}\\horses\\all`;
 
   constructor(private http: HttpClient) { }
 
   getAllRiders() : Observable<RidersView[]> {
     return this.http.get<RidersView[]>(this.ALL_RIDERS_URL);
+  }
+
+  getAllHorses() : Observable<Horse[]> {
+    return  this.http.get<Horse[]>(this.ALL_HORSES_URL);
   }
 }
